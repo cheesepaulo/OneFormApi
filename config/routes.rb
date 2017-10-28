de587c6fc606bedede7cda641157f26ca91b85d4
Paulo Cesar <pcdsjr@gmail.com>
2017-10-28 18:01:44 -0500

    create controller questions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
       resources :forms
+      resources :questions, only: [:create, :update, :destroy]
     end
   end
 end
