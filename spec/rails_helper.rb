2cdd8ec0ffe33fdef69b4cb5fc9a9fcc4767d8b6
Paulo Cesar <pcdsjr@gmail.com>
2017-10-29 10:49:47 -0500

    include custom helpers on rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,11 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 config.include Devise::Test::ControllerHelpers, type: :controller
 
+# Custom json helpers
+config.include Requests::JsonHelpers, type: :request
+# Custom Header helpers
+config.include Requests::HeaderHelpers, type: :request
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
