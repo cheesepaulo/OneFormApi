f5d3119fab02bb6d4b5bcd0d5ea0463fb52b6d4b
Paulo Cesar <pcdsjr@gmail.com>
2017-11-05 12:19:03 -0500

    include associoations and validations to form model

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,6 +1,11 @@
 class Form < ApplicationRecord
-  belongs_to :user
-
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  belongs_to :user
+  has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy
+
+  validates :title, :description, :user, presence: true
+  # validates :title, uniqueness: true
 end
