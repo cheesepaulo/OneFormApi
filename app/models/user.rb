50963db988ee8e126db081da5b6cb018e2fc2aba
Paulo Cesar <pcdsjr@gmail.com>
2017-11-05 12:19:54 -0500

    include has_many forms to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
-  include DeviseTokenAuth::Concerns::User
+  include DeviseTokenAuth::Concerns::
+  
+  has_many :forms, dependent: :destroy
 end
