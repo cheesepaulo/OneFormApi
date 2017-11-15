64032033b571930fdeac8bd420aaaed4981fbaef
Paulo Cesar <pcdsjr@gmail.com>
2017-11-15 11:18:08 -0500

    update user model to fix devise token auth concerns

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
-  include DeviseTokenAuth::Concerns::
+  include DeviseTokenAuth::Concerns::User
   
   has_many :forms, dependent: :destroy
 end
